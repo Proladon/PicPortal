0ccf44739712b413875c15c2c37a19ecccf60686
Proladon <proladon@gmail.com>
2021-08-15 18:02:01 +0800

    init

--- src/views/About.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="settings">Settings</div>
-</template>
-
-<style lang="postcss" scoped>
-.settings {
-  @apply text-gray-300 text-center w-full text-xl;
-}
-</style>
--- vue.config.ts
+++
@@ -1,8 +0,0 @@
-import path from 'path'
-module.exports = {
-  pluginOptions: {
-    electronBuilder: {
-      preload: path.join(__dirname, 'preload.ts')
-    }
-  }
-}
