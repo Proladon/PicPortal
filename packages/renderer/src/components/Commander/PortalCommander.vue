1b622d1ea91b63163081efccb73571138782a6aa
Proladon <Proladon@gmail.com>
2022-11-08 23:18:26 +0800

    [wip] portalCommander: select search

--- packages/renderer/src/components/Commander/PortalCommander.vue
+++ packages/renderer/src/components/Commander/PortalCommander.vue
@@ -1,18 +1,22 @@
 <template>
   <n-modal v-model:show="showModal" :on-update:show="updateModalShow">
     <div class="modal-body">
-      <n-input
-        v-model:value="searchPortalName"
-        autofocus
+      <input
+        class="search-input"
+        v-model="searchPortalName"
         :placeholder="translate('portalPane.search.placeholder')"
+        @keydown.up.prevent="onKeyUp"
+        @keydown.down.prevent="onKeyDown"
+        @keydown.enter.prevent="onSelect"
       />
 
-      <div class="mt-[10px]">
+      <div class="mt-[10px] h-[200px] overflow-y-auto">
         <div
+          class="portal-option"
           v-if="matchPrtals.length"
-          v-for="portal in matchPrtals"
+          v-for="(portal, index) in matchPrtals"
           :key="portal.id"
-          :class="isSelected(portal)"
+          :class="isSelected(index)"
         >
           {{ portal.name }}
         </div>
@@ -22,11 +26,12 @@
 </template>
 
 <script setup lang="ts">
-import { NModal, NInput } from 'naive-ui'
+import { NModal } from 'naive-ui'
 import { onMounted, ref, computed } from 'vue'
 import { useModal } from '/@/use/modal'
 import { usePortalPaneStore } from '/@/store/portalPaneStore'
 import useLocale from '/@/use/locale'
+import { find } from 'lodash-es'
 
 const emit = defineEmits(['close', 'confirm'])
 const portalPaneStore = usePortalPaneStore()
@@ -34,16 +39,46 @@ const { translate } = useLocale()
 const { updateModalShow, showModal } = useModal(emit)
 
 const searchPortalName = ref('')
+const selectIndex = ref(0)
 
 const matchPrtals = computed(() => {
+  if (searchPortalName.value === '*') return portalPaneStore.flattenPortals
   if (!searchPortalName.value.trim()) return []
-  return portalPaneStore.flattenPortals.filter((i) =>
-    i.name.includes(searchPortalName.value)
-  )
+  return portalPaneStore.flattenPortals.filter((i) => {
+    const name = i.name.toLowerCase()
+    const searchName = searchPortalName.value.toLowerCase()
+    return name.includes(searchName)
+  })
 })
 
-const isSelected = (portal: Portal) => {
-  return 'selected'
+const test = () => {
+  console.log(123)
+}
+
+const onKeyUp = () => {
+  if (selectIndex.value > 0) selectIndex.value--
+}
+const onKeyDown = () => {
+  if (selectIndex.value < matchPrtals.value.length - 1) selectIndex.value++
+}
+const onSelect = () => {
+  const portal: Portal = matchPrtals.value[selectIndex.value]
+  const portalGroup: PortalGroup | undefined = find(
+    portalPaneStore.portals,
+    (group) => find(group.childs, { id: portal.id })
+  )
+  if (portalGroup) {
+    portalPaneStore.AddActivedPortal({
+      id: portal.id,
+      group: portalGroup.id,
+    })
+  }
+  updateModalShow(false)
+}
+
+const isSelected = (index: number) => {
+  if (index === selectIndex.value) return 'selected'
+  return ''
 }
 
 onMounted(() => {
@@ -54,9 +89,18 @@ onMounted(() => {
 <style scoped lang="postcss">
 .modal-body {
   @apply bg-primary-bg p-5 min-w-[300px] rounded-lg;
+  @apply fixed top-[30%] left-0 right-0 w-[500px];
+}
+
+.portal-option {
+  @apply p-[3px] text-[18px];
 }
 
 .selected {
-  @apply bg-border text-darkBlue font-medium rounded-sm p-[3px];
+  @apply bg-border text-darkBlue rounded-sm;
+}
+
+.search-input {
+  @apply bg-transparent w-full border-none outline-none text-[30px];
 }
 </style>
